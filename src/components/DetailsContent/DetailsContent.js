b6407f228487d03932f131a97c032e506a2dab5d
unknown <ahmadmasriahmad@gmail.com>
2024-08-24 20:16:40 +0300

    Fixing some bugs

--- src/components/DetailsContent/DetailsContent.js
+++ src/components/DetailsContent/DetailsContent.js
@@ -2,9 +2,15 @@ import { Button } from '../button/Button';
 import classes from './DetailsContent.module.css'
 import { AiFillCaretDown, AiFillSetting, AiOutlineRedo } from "react-icons/ai";
 import { DetailsSection } from '../section/DetailsSection';
+import { useNavigate } from "react-router-dom";
 
 export const DetailsContent = () => {
 
+    const navigate = useNavigate();
+
+    const goBack = () => {
+        navigate('/');
+    }
     return <div className={classes.container}>
         <div className={classes.buttonsContainer}>
             <div className={classes.leftButtons}>
@@ -16,7 +22,7 @@ export const DetailsContent = () => {
 
             </div>
             <div className={classes.rightButtons}>
-                <Button title={'الرجوع'} styles={classes.whiteButton} />
+                <Button title={'الرجوع'} styles={classes.whiteButton} action={goBack} />
             </div>
         </div>
         <DetailsSection data={[]}/>
--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,9 +1,9 @@
 import classes from './Button.module.css';
 
-export const Button = ({ title, styles, icon, afterIcon }) => {
+export const Button = ({ title, styles, icon, afterIcon, action }) => {
 
     console.log(styles)
-    return <div className={`${classes.button} ${styles}`}>
+    return <div className={`${classes.button} ${styles}`} onClick={action}>
         {icon}
         <span style={{marginRight: afterIcon ? '5px' : 'auto'}}>
             {title}
